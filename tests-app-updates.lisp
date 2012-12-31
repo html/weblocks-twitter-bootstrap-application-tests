0a9879e68fe5133350b28d3f569b7af3c65b0be3
Olexiy Zamkoviy <olexiy.z@gmail.com>
2012-12-31 15:08:37 +0200

    Added some demo actions

--- tests-app-updates.lisp
+++ tests-app-updates.lisp
@@ -1,24 +1,13 @@
 (in-package :weblocks-twitter-bootstrap-application-tests)
 
-#+l(defun ajax-file-field-demonstration-action (&rest args)
-  (do-page 
-    (make-quickform 
-      (defview 
-        nil 
-        (:caption "Ajax file form field demo" :type form :persistp nil :enctype "multipart/form-data" :use-ajax-p t)
-        (file 
-          :present-as ajax-file-upload 
-          :parse-as (ajax-file-upload 
-                      :upload-directory (weblocks-selenium-tests-app::get-upload-directory)
-                      :file-name :unique)
-          :writer (lambda (value item)))))))
-
 (defvar *test-webapp-prefix*  "/bootstrap-app")
+
+(defstore *bootstrap-tests-store* :memory)
 
 (defwebapp twitter-bootstrap-sample-app
            :prefix *test-webapp-prefix*
            :description "Sample twitter bootstrap application"
-           :init-user-session 'weblocks-selenium-tests-app::init-user-session-prototype
+           :init-user-session 'init-user-session-bootstrap
            :subclasses (weblocks-twitter-bootstrap-application:twitter-bootstrap-webapp)
            :autostart t                   
            :ignore-default-dependencies nil 
@@ -39,5 +28,62 @@
             "weblocks-jquery/weblocks-jquery.js"
             (asdf-system-directory :weblocks-twitter-bootstrap-application-tests))) weblocks::*dispatch-table*)) 
 
-;(weblocks-selenium-tests-app::define-demo-action "Ajax file upload" #'ajax-file-field-demonstration-action :prototype-engine-p nil)
+(define-demo-action 
+  "Bootstrap theme demos" 
+  (lambda (&rest args)
+    (redirect *test-webapp-prefix* :defer nil))
+  :prototype-engine-p nil)
+
+(defvar *demo-actions* nil)
+
+(defun define-bootstrap-demo-action (link-name action &key (prototype-engine-p t) (jquery-engine-p t))
+  "Used to add action to demo list, 
+   :prototype-engine-p and :jquery-engine-p keys 
+   are responsive for displaying action in one or both demo applications"
+  (push (list link-name action prototype-engine-p jquery-engine-p) *demo-actions*))
+
+(define-bootstrap-demo-action "Dialog" #'weblocks-selenium-tests-app::dialog-demonstration-action)
+
+(defclass test-model ()
+  ((id) 
+   (title :accessor test-model-title :initarg :title) 
+   (content :accessor test-model-content :initarg :content)))
+
+(defun add-demo-records-to-test-model ()
+  (loop for i from 1 to 5 do 
+        (persist-object 
+          *bootstrap-tests-store* 
+          (make-instance 
+            'test-model
+            :title (format nil "Title-~A" i)
+            :content (format nil "Content-~A" i))))) 
+
+(defun init-user-session-bootstrap (root &optional (add-demo-records t))
+  (when add-demo-records 
+    (add-demo-records-to-test-model))
+  (setf (widget-children root)
+        (list (lambda (&rest args)
+                (with-html
+                  (:h1 "Twitter bootstrap for weblocks demo")
+                  (:ul
+                    (loop for (link-title action) in (reverse *demo-actions*) 
+                          do
+                          (cl-who:htm (:li (render-link action link-title))))))))))
+
+(defun gridedit-demonstration-action (&rest args)
+  (let* ((widget))
+    (setf widget (make-instance 'gridedit :data-class 'test-model))
+    (do-page 
+      (list 
+        (lambda (&rest args)
+          (with-html 
+            (:h1 "Gridedit")))
+        widget 
+        (lambda (&rest args)
+          (render-link 
+            (lambda (&rest args)
+              (init-user-session-bootstrap (root-widget) nil))
+            "Back"))))))
+
+(define-bootstrap-demo-action "Gridedit" #'gridedit-demonstration-action)
 
--- weblocks-twitter-bootstrap-application-tests.asd
+++ weblocks-twitter-bootstrap-application-tests.asd
@@ -5,7 +5,7 @@
   :description "Demo app for weblocks-twitter-bootstrap-application"
   :author "Olexiy Zamkoviy <olexiy.z@gmail.com>"
   :license "LLGPL"
-  :version "0.0.1"
+  :version "0.0.2"
   :depends-on (#:weblocks-twitter-bootstrap-application #:weblocks-selenium-tests)
   :components 
   ((:file "package")
