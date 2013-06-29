3613c68c6e8b4879678c3c990ff15c63c5f5f122
Olexiy Zamkoviy <olexiy.z@gmail.com>
2013-06-29 13:03:21 +0300

    Exported some symbols

--- package.lisp
+++ package.lisp
@@ -1,3 +1,4 @@
 (defpackage #:weblocks-twitter-bootstrap-application-tests
-  (:use #:cl #:weblocks #:weblocks-selenium-tests #:weblocks-twitter-bootstrap-application #:weblocks-selenium-tests-app #:selenium))
+  (:use #:cl #:weblocks #:weblocks-selenium-tests #:weblocks-twitter-bootstrap-application #:weblocks-selenium-tests-app #:selenium)
+  (:export #:with-new-or-existing-selenium-session-on-bootstrap-site #:define-bootstrap-demo-action))
 
